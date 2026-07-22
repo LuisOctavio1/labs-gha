0aff9bea78d7d45d9cfe022be84a6b215cf40925
Luis <octa9000@gmail.com>
2026-07-21 21:16:12 -0700

    fix terra

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -40,6 +40,8 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
+  enable_cluster_creator_admin_permissions = true
+
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
