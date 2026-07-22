995968b7c0a78298fcfb7344a7a14d699eb627c2
Luis <octa9000@gmail.com>
2026-07-21 19:31:42 -0700

    fixing terraform module

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -45,7 +45,7 @@ module "eks" {
       min_size       = 1
       max_size       = 2
       desired_size   = 1
-      instance_types = ["t2.micro"] 
+      instance_types = ["t3.small"] 
     }
   }
 }
