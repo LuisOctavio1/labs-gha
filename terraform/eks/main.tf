b3a8e038311ccf2efed7194bb5569b409bd3a51e
Luis <octa9000@gmail.com>
2026-07-21 18:00:09 -0700

    fixing terraform module

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -31,10 +31,10 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.0.0"
+  version = "~> 20.31"
 
   cluster_name    = "demo-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.31"    
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
@@ -42,9 +42,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
       instance_types = ["t3.medium"]
     }
   }
